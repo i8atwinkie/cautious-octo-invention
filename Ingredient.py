2f3740bb1d453d280067513ffbe2e9577e2dc076
Jayden Barwick <i8atwinkie@gmail.com>
2019-11-16 13:12:08 -0700

    ingredient price deafualt 0

--- Ingredient.py
+++ Ingredient.py
@@ -1,6 +1,6 @@
 class Ingredient:
 
-    def __init__(self, name = "", price = None, id = None):
+    def __init__(self, name = "", price = 0, id = None):
         self.price = price
         self.name = name
         self.id = id
