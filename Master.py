b2891a8856981deb7d4fa5c3e7c4751aa44b884a
Calvin <cvclark47@gmail.com>
2019-11-15 23:52:36 -0700

    Added ingredient class & getPrice functions

--- Master.py
+++ Master.py
@@ -1,3 +1,26 @@
+class ingredient:
+
+    def __init__(self):
+        self.__price
+    
+    def getPrice(self):
+        return self.__price
+
 class Meal: 
-    ingredients = []
+
+    def __init__(self):
+        self.__ingredients = []
+    
+    def getMealList(self):
+        return self.__ingredients
+        
+    def addIngredient(self, ingredient):
+        self.__ingredients.append(ingredient)
+
+    def getPrice(self):
+        price = 0
+        for i in self.__ingredients:
+            price +=i.getPrice
+        return price
+
 
