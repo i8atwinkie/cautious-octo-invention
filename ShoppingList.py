7d24a35d2e235f56dd3215bce07f44e11aaf0384
Jayden Barwick <i8atwinkie@gmail.com>
2019-11-16 08:39:31 -0700

    fixed shopping list

--- ShoppingList.py
+++ ShoppingList.py
@@ -4,7 +4,7 @@ from ShoppingListIngredient import ShoppingListIngredient
 class ShoppingList:
     def __init__(self, ingredients = []):
         self.shoppingListIngredients = []
-        for shoppingListIngredient in self.shoppingListIngredients:
+        for ingredient in ingredients:
             self.shoppingListIngredients.append(ShoppingListIngredient(ingredient))
 
     def getIngredients(self):
@@ -15,21 +15,21 @@ class ShoppingList:
 
     def getTotalPrice(self):
         total = 0
-        for shoppingListIngredient in shoppingListIngredients:
-            total += ingredient.getPrice
+        for shoppingListIngredient in self.shoppingListIngredients:
+            total += shoppingListIngredient.getPrice
         return total
 
     def getCheckedOffPrice(self):
         total = 0
-        for shoppingListIngredient in shoppingListIngredients:
-            if shoppingListIngredient.checkedOff is true:
-                total += ingredient.getPrice
+        for shoppingListIngredient in self.shoppingListIngredients:
+            if shoppingListIngredient.checkedOff is True:
+                total += shoppingListIngredient.getPrice
         return total
 
     def getCheckedOffIngredients(self):
         shoppingListIngredients = []
         for shoppingListIngredient in self.shoppingListIngredients:
-            if shoppingListIngredient.checkedOff is true:
+            if shoppingListIngredient.checkedOff is True:
                 shoppingListIngredients.append(shoppingListIngredient)
         return shoppingListIngredients
 
