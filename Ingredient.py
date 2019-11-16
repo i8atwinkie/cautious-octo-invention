f218b66d1c65d310885784e9699d4f522daeeb68
Jayden Barwick <i8atwinkie@gmail.com>
2019-11-16 07:28:23 -0700

    ingredient variables are no longer private

--- Ingredient.py
+++ Ingredient.py
@@ -4,8 +4,8 @@ class Ingredient:
 
 
     def __init__(self, name = "", price = 0):
-        self.__price = price
-        self.__name = name
+        self.price = price
+        self.name = name
         self.id = None
     
     def getPrice(self):
